62380eef9e92f906ccc81f5f0115b3d44cbb3cd1
Ankit Panwar <anshupanwar1801@gmail.com>
2024-09-03 16:38:46 +0530

    Update db_conn.py

--- my_dagster_project/io/db_conn.py
+++ my_dagster_project/io/db_conn.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 def get_sql_conn():
     """return db connection."""
     #get password from environmnet var
-    db_user = "sql12728197"
-    db_password = "7fz7Efym5T"
+    db_user = "sql12729223"
+    db_password = "NgT3dSRK1F"
     db_host = "sql12.freesqldatabase.com"
     db_port = 3306
-    db_name = "sql12728197"
+    db_name = "sql12729223"
     
     connection_string = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     #
